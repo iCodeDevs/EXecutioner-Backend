f11705b32e0ace2fbf1e061d8cd004f9225884c3
dravog7 <dravog78@gmail.com>
2021-09-19 15:30:11 +0000

    better installation

--- install.sh
+++ install.sh
@@ -1,16 +1,9 @@
 apt update
-apt install -y firejail
-apt install -y python3 python3-pip
-
-apt install -y gcc # for C
-
-# python3 -m pip install poetry
-
-# python3 -m poetry config virtualenvs.in-project false
-# python3 -m poetry install
+apt install -y firejail python3 python3-pip gcc
 
 python3 -m pip install -r requirements.txt # for very small instances
 
-python3 -m pip install pytest
 # change based on python version
-python3 -m pytest /usr/local/lib/python3.8/dist-packages/executioner/
+python3 -m pytest --disable-warnings /usr/local/lib/python3.8/dist-packages/executioner/
+# folder made using service is root owned
+mkdir playground
