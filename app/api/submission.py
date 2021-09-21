71e21e60547388d8b982599ef460f743be392d91
dravog7 <dravog78@gmail.com>
2021-09-21 07:44:32 +0000

    multiple submissions handling

--- app/api/submission.py
+++ app/api/submission.py
@@ -10,13 +10,13 @@ from app.deps import get_queue
 submission_router = APIRouter()
 
 
-async def run_program(websocket: WebSocket, queue: Queue, program: str, language: str, input_text: str):
+async def run_program(websocket: WebSocket, queue: Queue, program: str, language: str, input_text: str, id: str):
     with Program(program, language) as pgm:
         job = queue.enqueue(execute, pgm.to_json_object(),
                             TestCase(input_text).to_json_object())
         while not job.result:
             await asyncio.sleep(1)
-        await websocket.send_json({"command": "response", "result": job.result})
+        await websocket.send_json({"id": id, "command": "response", "result": job.result})
 
 
 @submission_router.websocket("/ws")
@@ -26,9 +26,19 @@ async def websocket_endpoint(websocket: WebSocket, queue: Queue = Depends(get_qu
     loop = asyncio.get_event_loop()
     while True:
         data = await websocket.receive_json()
+        if "id" not in data:
+            await websocket.send_json({"command": "error", "text": "missing id"})
         if data["command"] == "execute":
             if ("program" not in data) or ("language" not in data):
                 await websocket.send_json({"command": "error", "text": "invalid execute command"})
                 continue
-            loop.create_task(run_program(
-                websocket, queue, data["program"], data["language"], data.get("input", "")))
+            loop.create_task(
+                run_program(
+                    websocket,
+                    queue,
+                    data["program"],
+                    data["language"],
+                    data.get("input", ""),
+                    data["id"]
+                )
+            )
