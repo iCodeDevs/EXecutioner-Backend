b9f690ebf671794e1825a2d1bffe22d96df5ccc0
dravog7 <dravog78@gmail.com>
2021-09-29 10:22:03 +0000

    redis dashboard fixed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
 from flask import Flask
@@ -5,6 +6,8 @@ import rq_dashboard
 from .api import api_router
 
 rq_app = Flask(__name__)
+rq_app.config['REDIS_URL'] = os.environ.get('REDIS_URL')
+rq_app.config.from_object(rq_dashboard.default_settings)
 rq_app.register_blueprint(rq_dashboard.blueprint,url_prefix="/rq")
 
 app = FastAPI()
