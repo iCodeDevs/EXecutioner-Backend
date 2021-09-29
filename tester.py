526115c4dbb114edfeaf8fb08b32662d9329d878
dravog7 <dravog78@gmail.com>
2021-09-29 09:49:54 +0000

    rename path in  service related files

--- tester.py
+++ tester.py
@@ -12,15 +12,12 @@ if "REDIS_URL" in os.environ:
 
 q = Queue(connection=redis)
 pgm = Program('''
-#include<stdio.h>
-void main(){
-    int num;
-    scanf("%d",&num);
-    printf("%d",num);
-}''',"C")
-test = TestCase("-12492294823928402840294820482")
+print("hello world")
+''',"python3")
+test = TestCase()
 
 job = q.enqueue(tasks.execute,pgm.to_json_object(),test.to_json_object())
 time.sleep(1)
+print(job.result)
 test = TestCase.from_json_object(job.result)
 print(test.error,test.real_output)
